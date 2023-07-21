c69d8787917a150b385fb50fc6c0590072ebefba
Sachar04 <97813614+Sachar04@users.noreply.github.com>
2023-07-21 22:39:14 +0200

    Delete edsite directory

--- edsite/contact.js
+++
@@ -1,7 +0,0 @@
-const faqItems = document.querySelectorAll('.faq-list li');
-    
-faqItems.forEach(item => {
-  item.addEventListener('click', () => {
-    item.classList.toggle('active');
-  });
-});
